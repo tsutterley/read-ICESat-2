a6f20323ea28ac2ea563ecee981f1c07dc59ccce
tsutterley <tsutterl@uw.edu>
2023-03-09 11:05:07 -0800

    Update __init__.py

--- icesat2_toolkit/__init__.py
+++ icesat2_toolkit/__init__.py
@@ -22,18 +22,5 @@ import icesat2_toolkit.version
 from icesat2_toolkit.convert_delta_time import convert_delta_time
 from icesat2_toolkit import io
 
-# deprecated modules
-from icesat2_toolkit.read_ICESat2_ATL03 import read_HDF5_ATL03, \
-    find_HDF5_ATL03_beams, read_HDF5_ATL09, read_HDF5_ATL03_main, read_HDF5_ATL03_beam
-from icesat2_toolkit.read_ICESat2_ATL06 import read_HDF5_ATL06, \
-    find_HDF5_ATL06_beams, read_HDF5_ATL06_beam
-from icesat2_toolkit.read_ICESat2_ATL07 import read_HDF5_ATL07, \
-    find_HDF5_ATL07_beams
-from icesat2_toolkit.read_ICESat2_ATL10 import read_HDF5_ATL10, \
-    find_HDF5_ATL10_beams
-from icesat2_toolkit.read_ICESat2_ATL11 import read_HDF5_ATL11
-from icesat2_toolkit.read_ICESat2_ATL12 import read_HDF5_ATL12, \
-    find_HDF5_ATL12_beams
-
 # get version number
 __version__ = icesat2_toolkit.version.version
